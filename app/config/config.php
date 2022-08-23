55ade585d6e82db355766d705e578effec72e911
«Yergali <y.asbayev@jusan.kz>
2022-08-23 10:48:41 +0600

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,32 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'instanceid' => 'ocd9dcgljf7g',
+  'passwordsalt' => '1STHMEoSTT/3weo4OlUK/v+Hy7J/5w',
+  'secret' => 'CSxKq772jS+3E9ZC3DxNeWVaOr5q8aZSR5nuxQtoHEUS9FAF',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'sqlite3',
+  'version' => '24.0.4.1',
+  'overwrite.cli.url' => 'http://localhost',
+  'installed' => true,
+);
